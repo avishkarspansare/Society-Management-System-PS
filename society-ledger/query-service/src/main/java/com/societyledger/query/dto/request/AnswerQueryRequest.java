466b0bb650c6a8077227af9ed809f560caa8e517
Avishkar Pansare <87558645+avishkarspansare@users.noreply.github.com>
2026-06-28 18:04:55 +0530

    Delete society-ledger directory

--- society-ledger/query-service/src/main/java/com/societyledger/query/dto/request/AnswerQueryRequest.java
+++
@@ -1,9 +0,0 @@
-package com.societyledger.query.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-
-@Data
-public class AnswerQueryRequest {
-    @NotBlank String answer;
-}
