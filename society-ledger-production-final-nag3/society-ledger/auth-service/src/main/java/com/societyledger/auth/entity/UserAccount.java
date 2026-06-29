2dfb82f74c3cc8d605baa53b98b4dde7b05f9a90
Avishkar Pansare <avishkarspansare@gmail.com>
2026-06-29 18:49:57 +0000

    Jun 30, 2026, 12:19 AM

--- society-ledger-production-final-nag3/society-ledger/auth-service/src/main/java/com/societyledger/auth/entity/UserAccount.java
+++ society-ledger-production-final-nag3/society-ledger/auth-service/src/main/java/com/societyledger/auth/entity/UserAccount.java
@@ -49,9 +49,7 @@ public class UserAccount {
     @Column(name = "is_active")
     @Builder.Default
     private Boolean isActive = true;
-
     
-
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private Instant createdAt;
